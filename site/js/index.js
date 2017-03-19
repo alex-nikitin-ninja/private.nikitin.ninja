5ab123b8a7079ce6372a068567c86b447f5bd676
Alex <alex.nikitin.2015@gmail.com>
2017-03-19 02:32:22 -0700

    minor update

--- site/js/index.js
+++ site/js/index.js
@@ -2,6 +2,7 @@ var app = angular.module('app', ['ngRoute', 'ngAnimate', 'mgcrea.ngStrap']);
 
 app.config(function($routeProvider, $locationProvider) {
     $locationProvider.hashPrefix('');
+    // not supported by any of browsers fully yet
     // $locationProvider.html5Mode({
     //     enabled: true,
     //     requireBase: false
@@ -11,14 +12,17 @@ app.config(function($routeProvider, $locationProvider) {
             templateUrl: 'site/templates/body/index.html',
             controller: 'indexController'
         })
+
         .when('/about', {
             templateUrl: 'site/templates/body/about.html',
             controller: 'aboutController'
         })
+        
         .when('/test/:number', {
             templateUrl: 'site/templates/body/about.html',
             controller: 'aboutController'
         })
+        
         .otherwise({
             redirectTo: '/'
         });
