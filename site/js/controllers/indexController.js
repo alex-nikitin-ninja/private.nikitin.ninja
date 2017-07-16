acc52aff5681cfdb497fec2675b9ef4de7de1bbd
Alex <alex.nikitin.2015@gmail.com>
2017-07-16 13:27:09 -0700

    various updates

--- site/js/controllers/indexController.js
+++ site/js/controllers/indexController.js
@@ -1,5 +1,7 @@
 app.controller('indexController', function($scope, $$userDefinitions) {
     $scope.message = 'This is indexController message';
+    
+    $scope.userDefinitions = $$userDefinitions;
 
     // console.log(userDefinitions.getName());
 });
